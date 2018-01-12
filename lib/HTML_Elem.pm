ab6d79d2c3a05261520754afca6f6478d5273e33
suzutsuki0220 <suzutsuki0220@gmail.com>
2018-01-13 00:10:11 +0900

    modify charset metadata of HTML header

--- lib/HTML_Elem.pm
+++ lib/HTML_Elem.pm
@@ -27,7 +27,7 @@ sub header {
 <!DOCTYPE html>
 <html lang="ja">
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<meta charset="utf-8" />
 <meta name="viewport" content="width=device-width; initial-scale=1.0;">
 <meta http-equiv="Content-Script-Type" content="text/javascript">
 <meta http-equiv="Content-Style-Type" content="text/css">
@@ -147,7 +147,7 @@ sub header_smp {
 <!DOCTYPE html>
 <html lang="ja">
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<meta charset="utf-8" />
 <meta name="viewport" content="width=device-width: initial-scale=1.0; maximum-scale=1.0; user-scalable=no;">
 <meta name="apple-mobile-web-app-capable" content="yes">
 <meta name="apple-mobile-web-app-status-bar-style" content="black">
@@ -292,7 +292,7 @@ sub redirect
   print <<EOD;
 <html>
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<meta charset="utf-8" />
 <meta http-equiv="Content-Script-Type" content="text/javascript">
 <meta http-equiv="Content-Style-Type" content="text/css">
 <meta http-equiv="Refresh" content="${refresh}; url=${url}">
