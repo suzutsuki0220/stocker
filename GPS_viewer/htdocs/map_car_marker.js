a7495d321385da1f94f9743d424c903ad8d8e9b6
suzutsuki0220 <suzutsuki0220@gmail.com>
2018-10-21 01:01:56 +0900

    refactor: divide make new marker func

--- GPS_viewer/htdocs/map_car_marker.js
+++ GPS_viewer/htdocs/map_car_marker.js
@@ -20,22 +20,26 @@ mapCarMarker.prototype.getPosition = function() {
 mapCarMarker.prototype.setVisible = function(sw) {
     if (sw === true) {
         if (this.ins_car_mark === null) {
-            const latlng = this.marker_position ? this.marker_position : this.ins_map.getCenter();
-            const markerImage = {
-                url: CAR_MARKER_ICON,
-                size: new google.maps.Size(48,48),  // マーカー画像サイズ
-                origin: new google.maps.Point(0,0),
-                anchor: new google.maps.Point(24,24),
-            };
-            const options = {
-                icon: markerImage,
-                map: this.ins_map,
-                position: latlng,
-            };
-            this.ins_car_mark = new google.maps.Marker(options);
+            this.__makeNewMarker();
         }
     } else {
         this.ins_car_mark.setMap(null);
         this.ins_car_mark = null;
     }
+};
+
+mapCarMarker.prototype.__makeNewMarker = function() {
+    const latlng = this.marker_position ? this.marker_position : this.ins_map.getCenter();
+    const markerImage = {
+        url: CAR_MARKER_ICON,
+        size: new google.maps.Size(48,48),  // マーカー画像サイズ
+        origin: new google.maps.Point(0,0),
+        anchor: new google.maps.Point(24,24),
+    };
+    const options = {
+        icon: markerImage,
+        map: this.ins_map,
+        position: latlng,
+    };
+    this.ins_car_mark = new google.maps.Marker(options);
 };
