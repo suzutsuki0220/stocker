a3911ab1908865f461df341874d9f29b58d80fac
suzutsuki0220 <suzutsuki0220@gmail.com>
2018-10-24 15:38:10 +0900

    refactar: modify graph behavior to collect constant valiables

--- GPS_viewer/htdocs/map_graph_behavior.js
+++ GPS_viewer/htdocs/map_graph_behavior.js
@@ -6,21 +6,17 @@ var graphBehavior = function(canvas) {
     this.background_color = "#333333";
     this.graph_data = new Array();
 
-    var ctx = this.canvas.getContext('2d');
     this.canvas.width = 610;
     this.canvas.height = 40;
 
     const behavior_text = ["Er", "St", "B", "T", "L", "R"];
-    const graph_Y_step = this.canvas.height / this.behavior_types;
-    const graph_width  = this.canvas.width - this.graph_px_offset_start - this.graph_px_offset_start;
-    const graph_height = Math.floor(graph_Y_step * 0.75);
+    this.graph_Y_step = this.canvas.height / this.behavior_types;
+    this.graph_width  = this.canvas.width - this.graph_px_offset_start - this.graph_px_offset_start;
+    this.graph_height = Math.floor(this.graph_Y_step * 0.75);
 
-    ctx.beginPath();
-    ctx.lineWidth = 1;
-    ctx.setLineDash([0]);
-    ctx.font = "9px 'Avenir','Corbel','Osaka','MS Pゴシック',sans-serif";
+    var ctx = this.__makeCanvasContext();
     for (var i=0; i<this.behavior_types; i++) {
-        const startY = Math.floor(graph_Y_step * i);
+        const startY = Math.floor(this.graph_Y_step * i);
         ctx.strokeStyle = "#cccccc";
         ctx.fillStyle = "#cccccc";
         ctx.fillText(behavior_text[i], 10, startY + 7);
@@ -28,7 +24,7 @@ var graphBehavior = function(canvas) {
 
         ctx.strokeStyle = this.background_color;
         ctx.fillStyle = this.background_color;
-        ctx.fillRect(this.graph_px_offset_start, startY, graph_width, graph_height);
+        ctx.fillRect(this.graph_px_offset_start, startY, this.graph_width, this.graph_height);
         ctx.stroke();
     }
     ctx.closePath();
@@ -40,6 +36,17 @@ graphBehavior.prototype.push = function(position) {
     e.behavior = position.behavior;
     e.level    = position.level;
     this.graph_data.push(e);
+};
+
+graphBehavior.prototype.__makeCanvasContext = function() {
+    var ctx = this.canvas.getContext('2d');
+
+    ctx.beginPath();
+    ctx.lineWidth = 1;
+    ctx.setLineDash([0]);
+    ctx.font = "9px 'Avenir','Corbel','Osaka','MS Pゴシック',sans-serif";
+
+    return ctx;
 };
 
 graphBehavior.prototype.__isMatchBehavior = function(data, behavior) {
@@ -54,15 +61,15 @@ graphBehavior.prototype.__isMatchBehavior = function(data, behavior) {
     return ret;
 };
 
-graphBehavior.prototype.__getLevelInRange = function(start, end, graph_data, behavior) {
+graphBehavior.prototype.__getEventLevelInRange = function(start, end, behavior) {
     var max_level = 0;
 
     for (var i=start; i<=end; i++) {
-        if (this.__isMatchBehavior(graph_data[i], behavior) === false) {
+        if (this.__isMatchBehavior(this.graph_data[i], behavior) === false) {
             continue;
         };
 
-        const level = isNaN(graph_data[i].level) === true ? 1 : graph_data[i].level;
+        const level = isNaN(this.graph_data[i].level) === true ? 1 : this.graph_data[i].level;
         if (level > max_level) {
             max_level = level;
             if (max_level === 3) {
@@ -74,16 +81,22 @@ graphBehavior.prototype.__getLevelInRange = function(start, end, graph_data, beh
     return max_level;
 };
 
-graphBehavior.prototype.__hasStopInRange = function(start, end, graph_data) {
+graphBehavior.prototype.__hasStopInRange = function(start, end) {
     var level = 0;
 
+    const cond = [
+        {scene: "slight", level: 99},  // else level
+        {scene: "stop",   level:  1}
+    ];
+
     for (var i=start; i<=end; i++) {
-        if (graph_data[i] && graph_data[i].scene) {
-            if (graph_data[i].scene === "slight") {
-                level = 99;  // else level
-                break;
-            } else if (graph_data[i].scene === "stop") {
-                level = 1;
+        if (!this.graph_data[i] || !this.graph_data[i].scene) {
+            continue;
+        }
+
+        for (var j=0; j<cond.length; j++) {
+            if (this.graph_data[i].scene === cond[j].scene) {
+                level = cond[j].level;
                 break;
             }
         }
@@ -92,27 +105,34 @@ graphBehavior.prototype.__hasStopInRange = function(start, end, graph_data) {
     return level;
 };
 
-graphBehavior.prototype.__hasErrorInRange = function(start, end, graph_data) {
+graphBehavior.prototype.__hasErrorInRange = function(start, end) {
     var sample_count = 0;
     var error_count = 0;
     var level = 0;
 
+    const cond = [
+        {error_rate: 0.8, level: 3},
+        {error_rate: 0.4, level: 2},
+        {error_rate: 0,   level: 1}
+    ];
+
     for (var i=start; i<=end; i++) {
-        if (graph_data[i] && graph_data[i].scene) {
-            sample_count++;
-            if (graph_data[i].scene === "error") {
-                error_count++;
-            }
+        if (!this.graph_data[i] || !this.graph_data[i].scene) {
+            continue;
+        }
+
+        sample_count++;
+        if (this.graph_data[i].scene === "error") {
+            error_count++;
         }
     }
 
     const error_rate = sample_count !== 0 ? error_count / sample_count : 0;
-    if (error_rate > 0.8) {
-        level = 3;
-    } else if (error_rate > 0.4) {
-        level = 2;
-    } else if (error_count !== 0) {
-        level = 1;
+    for (var i=0; i<cond.length; i++) {
+        if (error_rate > cond[i].error_rate) {
+            level = cond[i].level;
+            break;
+        }
     }
 
     return level;
@@ -129,48 +149,46 @@ graphBehavior.prototype.__getStrokeColor = function(level) {
     return colors[color_index];
 };
 
+graphBehavior.prototype._getLevel = function(start, end, row_num) {
+    var level;
+
+    if (row_num === 0) {
+        level = this.__hasErrorInRange(start, end);
+    } else if (row_num === 1) {
+        level = this.__hasStopInRange(start, end);
+    } else {
+        const behavior = Math.pow(2, row_num - 2); // row_num-2: error, stopを除く
+        level = this.__getEventLevelInRange(start, end, behavior);
+    }
+
+    return level;
+};
+
 graphBehavior.prototype.plot = function() {
     if (this.graph_data.length === 0) {
         return;
     }
 
-    const graph_Y_step = this.canvas.height / this.behavior_types;
-    const graph_width  = this.canvas.width - this.graph_px_offset_start - this.graph_px_offset_end;
-    const graph_height = Math.floor(graph_Y_step * 0.75);
-
-    var ctx = this.canvas.getContext('2d');
-    ctx.beginPath();
-    ctx.lineWidth = 1;
-    ctx.setLineDash([0]);
-
-    const step = normalize(Math.floor(graph_width / this.graph_data.length), 2, graph_width);
-    for (var i=0; i<graph_width; i+=step) {
+    var ctx = this.__makeCanvasContext();
+    const step = normalize(Math.floor(this.graph_width / this.graph_data.length), 2, this.graph_width);
+    for (var i=0; i<this.graph_width; i+=step) {
         const x = this.graph_px_offset_start + i;
-        const start_pos = i / graph_width;
-        const end_pos   = (i+step) / graph_width;
+        const start_pos = i / this.graph_width;
+        const end_pos   = (i+step) / this.graph_width;
         const start = Math.floor(this.graph_data.length * start_pos);
         const end   = Math.floor(this.graph_data.length * end_pos);
 
         for (var j=0; j<this.behavior_types; j++) {
-            var level;
-            if (j === 0) {
-                level = this.__hasErrorInRange(start, end, this.graph_data);
-            } else if (j === 1) {
-                level = this.__hasStopInRange(start, end, this.graph_data);
-            } else {
-                var behavior = Math.pow(2, j - 2); // j-2: error, stopを除く
-                level = this.__getLevelInRange(start, end, this.graph_data, behavior);
-            }
+            const level = this._getLevel(start, end, j);
             if (level !== 0) {
-                const y = Math.floor(graph_Y_step * j);
-                const endX = i + step <= graph_width ? step : graph_width - i;
+                const y = Math.floor(this.graph_Y_step * j);
+                const endX = i + step <= this.graph_width ? step : this.graph_width - i;
                 ctx.strokeStyle = this.__getStrokeColor(level);
                 ctx.fillStyle = this.__getStrokeColor(level);
-                ctx.fillRect(x, y, endX, graph_height);
+                ctx.fillRect(x, y, endX, this.graph_height);
                 ctx.stroke();
             }
         }
     }
-
     ctx.closePath();
 };
