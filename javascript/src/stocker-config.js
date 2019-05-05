fb771f5dfb746041db7b4cd5ea7b8fa22fbf64a2
suzutsuki0220 <suzutsuki0220@gmail.com>
2019-05-05 21:58:41 +0900

    refactor: add configuration of cgi uri path

--- javascript/src/stocker-config.js
+++ javascript/src/stocker-config.js
@@ -1,12 +1,32 @@
 const CGI_ROOT = "/cgi-bin/stocker";
 
-const CGI_URI = {
+const CONVERTER_URI = {
+    selector: CGI_ROOT + '/convertselect.cgi',
+    movie_img: CGI_ROOT + '/get_movieimg.cgi',
+    movie_info: CGI_ROOT + '/movie_info'
+};
+
+const PICTURE_VIEWER_URI = {
+    get_picture: CGI_ROOT + '/get_picture.cgi',
+    exif_info: CGI_ROOT + '/exif_info'
+};
+
+const MUSIC_PLAYER_URI = {
+    get_media: CGI_ROOT + '/get_media.cgi',
+    tag_info: CGI_ROOT + '/taginfo'
+};
+
+const URI = {
     stocker: CGI_ROOT + '/stocker.cgi',
     get_dir: CGI_ROOT + '/get_dir',
-    get_file: CGI_ROOT + '/get_file'
+    get_file: CGI_ROOT + '/get_file',
+    thumbnail: CGI_ROOT + '/thumbnail.cgi',
+    converter: CONVERTER_URI,
+    picture_viewer: PICTURE_VIEWER_URI,
+    music_player: MUSIC_PLAYER_URI
 };
 
 module.exports.stockerConfig = {
     cgi_root: CGI_ROOT,
-    uri: CGI_URI
+    uri: URI
 };
