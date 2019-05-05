e31b26cb41fd740ce762e360b2b110597c118c1c
suzutsuki0220 <suzutsuki0220@gmail.com>
2019-05-05 18:24:32 +0900

    refactor: get_dir and get_file to stocker-config.js

--- javascript/src/stocker-config.js
+++ javascript/src/stocker-config.js
@@ -1,3 +1,12 @@
+const CGI_ROOT = "/cgi-bin/stocker";
+
+const CGI_URI = {
+    stocker: CGI_ROOT + '/stocker.cgi',
+    get_dir: CGI_ROOT + '/get_dir',
+    get_file: CGI_ROOT + '/get_file'
+};
+
 module.exports.stockerConfig = {
-    cgi_root: "/cgi-bin/stocker"
+    cgi_root: CGI_ROOT,
+    uri: CGI_URI
 };
