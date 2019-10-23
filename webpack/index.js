67f4cd0e8bd27a37a1711fa333cc68c1dffbfea1
suzutsuki0220 <suzutsuki0220@gmail.com>
2019-10-23 10:43:08 +0900

    refactor: remove stocker components js

--- webpack/index.js
+++ webpack/index.js
@@ -4,4 +4,3 @@ window.stocker = require('./src');
 
 // TODO: remove below
 window.stockerConfig = require('./src/stocker-config').stockerConfig;
-window.stockerComponents = require('./src/stocker_components.js');
