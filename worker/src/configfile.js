2f11d0a2f68d68e8b93ecf33fe4604aac60f7277
suzutsuki0220 <suzutsuki0220@gmail.com>
2020-04-22 00:57:53 +0900

    feat: add config file loader for js

---
+++ worker/src/configfile.js
@@ -0,0 +1,56 @@
+const fs = require('fs');
+
+const configPath = process.env.STOCKER_CONF;
+
+const kvPattern = /^[\s$]*(.*?)\s*=["' ]*(.*?)["'; ]*$/;
+
+function getKeyValue(line) {
+    if (!line) {
+        return null;
+    }
+
+    let comment = line.indexOf('#');
+    const lineEnd = comment >= 0 ? comment : line.length;
+
+    const kv = line.substring(0, lineEnd);
+    const match = kv.match(kvPattern);
+    if (!match) {
+        return null;
+    }
+
+    return [
+        match[1].toLowerCase(),
+        match[2]
+    ];
+}
+
+function loadConfig(data) {
+    let ret = new Object();
+
+    let sp = 0;
+    while(data && sp < data.length) {
+        let ep = data.indexOf('\n', sp);
+        if (ep < 0) {
+            ep = data.length;
+        }
+
+        const kv = getKeyValue(data.substring(sp, ep));
+        if (kv) {
+            ret[kv[0]] = kv[1];
+        }
+
+        sp = ep + 1;
+    }
+
+    return ret;
+}
+
+module.exports.load = function() {
+    if (!configPath) {
+        throw "failed to get STOCKER_CONF environment value";
+    }
+
+    const buffer = fs.readFileSync(configPath + '/converter.conf', 'utf8');
+
+    console.log(loadConfig(buffer));
+};
