32efb37f4cbaf557d2a4d37fa0a0eaa09dcb7e2d
suzutsuki0220 <suzutsuki0220@gmail.com>
2021-06-04 11:50:06 +0900

    fix: build script

---
+++ container/webui/make/libs/x265/build-x265.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+if [ ! -d x265 ]; then
+    hg clone http://hg.videolan.org/x265
+fi
+
+cd ./x265/build/linux
+cmake -G "Unix Makefiles" ../../source \
+    -DCMAKE_INSTALL_PREFIX=/usr \
+    -DHIGH_BIT_DEPTH=ON \
+    -DLIB_INSTALL_DIR=lib64
+make -j`nproc`
