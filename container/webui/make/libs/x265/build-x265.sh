48fcd308f1d6d1336d7e5ca9f7b3030ef01aca87
suzutsuki0220 <suzutsuki0220@gmail.com>
2021-06-05 23:14:54 +0900

    add AV encode library scripts

--- container/webui/make/libs/x265/build-x265.sh
+++ container/webui/make/libs/x265/build-x265.sh
@@ -8,5 +8,5 @@ cd ./x265/build/linux
 cmake -G "Unix Makefiles" ../../source \
     -DCMAKE_INSTALL_PREFIX=/usr \
     -DHIGH_BIT_DEPTH=ON \
-    -DLIB_INSTALL_DIR=lib64
+    -DLIB_INSTALL_DIR=lib
 make -j`nproc`
